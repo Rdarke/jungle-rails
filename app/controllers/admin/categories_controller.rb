7ec8535da0b401953dc313cc223edc6b5dbba78d
Rdarke <rickydarke1@gmail.com>
2021-10-14 15:58:30 -0700

    Create admin/categories routes & controller

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,14 @@
+class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["ADMIN_PRODUCTS_USERNAME"], password: ENV["ADMIN_PRODUCTS_PASSWORD"]
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+end
