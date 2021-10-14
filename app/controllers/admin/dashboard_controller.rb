cff37f21d9282ddf9e25712b2346cf27f12a06a3
Rdarke <rickydarke1@gmail.com>
2021-10-14 10:01:35 -0700

    Update dashboard controller to basic HTTP OAuth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  
+  http_basic_authenticate_with name: ENV["ADMIN_PRODUCTS_USERNAME"], password: ENV["ADMIN_PRODUCTS_PASSWORD"]
+
   def show
   end
 end
