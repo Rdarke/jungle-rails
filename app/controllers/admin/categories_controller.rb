af67b29c668b061418a5440a68f94eb209ad5e06
Rdarke <rickydarke1@gmail.com>
2021-10-14 16:28:23 -0700

    Render categories details

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,6 +3,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_PRODUCTS_USERNAME"], password: ENV["ADMIN_PRODUCTS_PASSWORD"]
 
   def index
+    @categories = Category.all
   end
 
   def new
