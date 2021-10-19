511c2388b69250c19fc8f5f82c3d5e0147dc5b8f
Rdarke <rickydarke1@gmail.com>
2021-10-19 14:43:52 -0700

    Update first user_spec validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,79 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    
+    it "credentials are present & valid" do
+      user = User.new(
+        first_name: 'User',
+        last_name: 'One',
+        email: 'user1@test.com',
+        password: 'testing',
+        password_confirmation: 'testing'
+      )
+      expect(user).to be_valid
+    end
+
+    it "should not save when first name is invalid" do
+      user = User.new(first_name: nil)
+      expect(user).to be_invalid
+    end
+
+    it "should not save when last name is invalid" do
+      user = User.new(last_name: nil)
+      expect(user).to be_invalid
+    end
+
+    it "should not save when email is invalid" do
+      user = User.new(email: nil)
+      expect(user).to be_invalid
+    end
+
+    it "should be invalid if password do not match" do
+      user = User.new(
+        first_name: 'User',
+        last_name: 'One',
+        email: 'user1@test.com',
+        password: 'testing',
+        password_confirmation: 'notTheSame'
+      )
+      user.valid?
+      expect(user.errors[:password_confirmation]).to be_present
+    end
+
+    it 'user should hold a unique email' do
+      user1 = User.new(
+        first_name: 'User',
+        last_name: 'One',
+        email: 'user1@test.com',
+        password: 'testing',
+        password_confirmation: 'testing'
+      )
+
+      user1.save
+    
+      user2 = User.new(
+        first_name: 'User',
+        last_name: 'Two',
+        email: 'user1@test.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      user2.save
+    
+      expect(user2.errors[:email].first).to eq('has already been taken')
+    end
+
+    it 'should have 6 or more characters within the password' do
+      user = User.new(
+        first_name: 'User',
+        last_name: 'One',
+        email: 'user1@test.com',
+        password: 'test',
+        password_confirmation: 'test'
+      )
+      expect(user).to be_invalid
+    end
+
+  end
 end
