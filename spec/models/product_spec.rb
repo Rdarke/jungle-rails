92b3717381d0b68f9e7bf8e7299e05fa403e3ef5
Rdarke <rickydarke1@gmail.com>
2021-10-18 19:20:14 -0700

    Add testing blocks to product_spec.rb

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @category = Category.new(name: 'Apparel')
+    @product = Product.new(name: 'Hipster Shirt', price_cents: 5000000, quantity: 5, category: @category)
+  end
+
+  describe 'Validations' do
+    it 'should save a valid product' do
+      @product.save
+    end
+
+    it 'should not save without a valid name' do
+      @product.name = nil
+      expect(@product.valid?).to be false 
+    end
+
+    it 'should not save without a valid quantity' do
+      @product.quantity = nil
+      expect(@product.valid?).to be false
+    end
+
+    it 'should not save based on a missing price' do
+      @product.price_cents = nil
+      expect(@product.valid?).to be false
+    end
+
+    it 'should not save based on a missing category' do
+      @product.category = nil
+      expect(@product.valid?).to be false
+    end
+  end
 end
